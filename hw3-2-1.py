188ff9cc3cd635ac5f7a5c0e0249e6f46933a72a
p22jdiao <p22jdiao@fairfieldprep.org>
2021-09-30 10:48:36 -0400

    3-2-1

--- hw3-2-1.py
+++ hw3-2-1.py
@@ -7,4 +7,32 @@ if x > 100:
 else:
     if x >= 93:
         print("A")
-    
+    else:
+        if x >= 90:
+            print("A-")
+        else:
+            if x >= 87:
+                print("B+")
+            else:
+                if x >= 83:
+                    print("B")
+                else:
+                    if x >= 80:
+                        print("B-")
+                    else:
+                        if x >= 77:
+                            print("C+")
+                        else:
+                            if x >= 73:
+                                print("C")
+                            else:
+                                if x >= 70:
+                                    print("C-")
+                                else:
+                                    if x >= 65:
+                                        print("D+")
+                                    else:
+                                        if x >= 60:
+                                            print("D")
+                                        else:
+                                            print("F")
