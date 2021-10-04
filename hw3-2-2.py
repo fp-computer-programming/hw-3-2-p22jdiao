f9e2a33c2be040fdd8a837f2c65d1e43eaf2f85a
p22jdiao <p22jdiao@fairfieldprep.org>
2021-10-04 10:04:01 -0400

    3-2

--- hw3-2-2.py
+++ hw3-2-2.py
@@ -10,3 +10,9 @@ if bmi < 19:
     print("Underweight")
 elif bmi < 25:
     print("Healthy")
+elif bmi < 30:
+    print("Overweight")
+elif bmi < 40:
+    print("Obese")
+else:
+    print("Extremly Obese")
